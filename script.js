3bf1f5e568fff632e0d30a16be29874edbbea31d
iamshbr <shabbirzohair@outlook.com>
2025-01-08 19:35:57 +0100

    First Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+"use-strict";
+
+const buttons = document.querySelectorAll(".button");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", function () {
+    document.body.style.backgroundColor = button.id;
+  })
+})
